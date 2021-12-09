7761158ababe6311ada0fc028f30839c32bc8519
jk <joern.kreutel@beuth-hochschule.de>
2021-12-09 18:28:20 +0100

    add questions and references related to Open API

--- src/main/resources/archetype-resources/src/main/java/crud/__entitytypeName__CRUDOpenAPIResource.java
+++ src/main/resources/archetype-resources/src/main/java/crud/__entitytypeName__CRUDOpenAPIResource.java
@@ -15,6 +15,10 @@ import javax.ws.rs.core.MediaType;
 /*
  * CRUD Ressource, die fuer die OpenAPI Beschreibung der server-seitigen WebAPI beruecksichtigt wird
  * (derzeit scheinen Ressourcen bei Trennung von Interface und Implementierung ignoriert zu werden)
+ *
+ * Wozu dient die OpenAPI Beschreibungssprache, wie ist ein OpenAPI Dokument aufgebaut? (siehe OPI:24 und 31-32)
+ * Welche Aspekte eines Java Datenmodells koennen mit OpenAPI nicht oder nur partiell ausgedrueckt werden? (siehe OPI:34-35)
+ * Welchen Vorteil bietet die Beschreibung eines Service Interfaces mittels OpenAPI verglichen mit der Beschreibung mit JAX-RS? (siehe: OPI:18-21 und 26)
  */
 @ApplicationScoped
 @Transactional
